a2b9b79c42357323b0c50a3ffcdb685a12357adc
Callum Scott <callumdscott@protonmail.com>
2020-12-14 19:36:31 +0000

    added TODO

--- sample/recognition.py
+++ sample/recognition.py
@@ -15,7 +15,8 @@ class Recogniser():
     def add_first_person(self, image):
         self.count = 1
         image = image[:, :, ::-1]
-        self.known_face_encodings.append(face_recognition.face_encodings(image)[0])
+        self.face_locations = face_recognition.face_locations(image)
+        self.known_face_encodings.append(face_recognition.face_encodings(image, self.face_locations)[0])
         self.known_face_names.append("face 1")
         self.labelled_faces.append("face 1")
 
@@ -23,20 +24,31 @@ class Recogniser():
         image = image[:, :, ::-1]
         self.face_locations = face_recognition.face_locations(image)
         self.face_encodings = face_recognition.face_encodings(image, self.face_locations)[0]
+        self.count += 1
+        # TODO: FIX BROKEN COMPARISON
+        #for face_encoding in self.face_encodings:
+        #    same_face = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
 
-        for face_encoding in self.face_encodings:
-            self.count = len(self.known_face_encodings) + 1
-            name = ""
-            same_face = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
+        #    if True in same_face:
+        #        print("found a match!")
+        #        match_index = same_face.index(True)
+        #        name = self.known_face_names[match_index]
+        #    else:
+        #        print("no match found...")
+        #        name = "face " + str(self.count)
+        #        self.known_face_encodings.append(self.face_encodings)
+        #        self.known_face_names.append(name)
 
-            if True in same_face:
-                match_index = same_face.index(True)
-                name = self.known_face_encodings[match_index]
+        face_distances = face_recognition.face_distance(self.known_face_encodings, self.face_encodings)
+        for i, face_distance in enumerate(face_distances):
+            if face_distance < 0.6:
+                print("found a match!")
+                name = self.known_face_names[i]
             else:
+                print("no match found...")
                 name = "face " + str(self.count)
                 self.known_face_encodings.append(self.face_encodings)
                 self.known_face_names.append(name)
-
         self.labelled_faces.append(name)
 
     def get_labelled_list(self):
