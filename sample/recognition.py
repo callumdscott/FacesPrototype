b7dc3b8fe609990563190389c13abe1059d21960
Callum Scott <callumdscott@protonmail.com>
2020-12-15 16:02:07 +0000

    added TODO - still broken

--- sample/recognition.py
+++ sample/recognition.py
@@ -2,54 +2,51 @@ import face_recognition
 import numpy as np
 
 
+def name_generator():
+    name = "face "
+    num = 1
+    while True:
+        yield name + str(num)
+        num += 1
+
 class Recogniser():
 
     def __init__(self):
-        self.count = 0
         self.known_face_encodings = []
         self.known_face_names = []
-        self.face_locations = []
-        self.face_encodings = []
         self.labelled_faces = []
+        self.generate = name_generator()
 
     def add_first_person(self, image):
-        self.count = 1
         image = image[:, :, ::-1]
-        self.face_locations = face_recognition.face_locations(image)
-        self.known_face_encodings.append(face_recognition.face_encodings(image, self.face_locations)[0])
-        self.known_face_names.append("face 1")
-        self.labelled_faces.append("face 1")
+        name = next(self.generate)
+        face_locations = face_recognition.face_locations(image)
+        self.known_face_encodings.append(face_recognition.face_encodings(image, face_locations)[0])
+        self.known_face_names.append(name)
+        self.labelled_faces.append(name)
 
     def add_person(self, image):
         image = image[:, :, ::-1]
-        self.face_locations = face_recognition.face_locations(image)
-        self.face_encodings = face_recognition.face_encodings(image, self.face_locations)[0]
-        self.count += 1
+        face_locations = face_recognition.face_locations(image)
+        face_encodings = face_recognition.face_encodings(image, face_locations)[0]
+        #self.count += 1
+        same_face =[]
         # TODO: FIX BROKEN COMPARISON
-        #for face_encoding in self.face_encodings:
-        #    same_face = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
+        for face_encoding in face_encodings:
+            same_face = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
+            print(same_face)
+        if True in same_face:
+            print("found a match!!!!!!")
+            index = same_face.index(True)
+            self.labelled_faces.append(self.known_face_names[index])
+        else:
+            print("no match")
+            name = next(self.generate)
+            self.known_face_encodings.append(face_encodings)
+            self.known_face_names.append(name)
+            self.labelled_faces.append(name)
 
-        #    if True in same_face:
-        #        print("found a match!")
-        #        match_index = same_face.index(True)
-        #        name = self.known_face_names[match_index]
-        #    else:
-        #        print("no match found...")
-        #        name = "face " + str(self.count)
-        #        self.known_face_encodings.append(self.face_encodings)
-        #        self.known_face_names.append(name)
-
-        face_distances = face_recognition.face_distance(self.known_face_encodings, self.face_encodings)
-        for i, face_distance in enumerate(face_distances):
-            if face_distance < 0.6:
-                print("found a match!")
-                name = self.known_face_names[i]
-            else:
-                print("no match found...")
-                name = "face " + str(self.count)
-                self.known_face_encodings.append(self.face_encodings)
-                self.known_face_names.append(name)
-        self.labelled_faces.append(name)
 
     def get_labelled_list(self):
         return self.labelled_faces
+
